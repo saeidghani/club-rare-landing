df011d792dbab81b3cbd6e7b3bd66aab5a6a3c76
saeid <saeidghani@gmail.com>
2021-06-29 12:58:07 +0430

    modified tailwind config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
+import React from "react";
 import "rc-drawer/assets/index.css";
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
